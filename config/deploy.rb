81b92c879b397a86e066c3a69b9cad77762c29f5
Paul <paul@conicrea.com>
2014-03-28 16:30:56 +0100

    mmm

--- config/deploy.rb
+++ config/deploy.rb
@@ -86,24 +86,6 @@ namespace :deploy do
     CMD
   end
 
-  # namespace :assets do
-  #   task :precompile, :roles => :web, :except => { :no_release => true } do
-  #     from = source.next_revision(current_revision) rescue nil
-  #     if from.nil? || capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
-  #       run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
-  #     else
-  #       logger.info "Skipping asset pre-compilation because there were no asset changes"
-  #     end
-  #   end
-  # end
-
-  namespace :paperclip do
-    desc 'Build missing paperclip styles.'
-    task :build_missing_paperclip_styles, roles: :app do
-      run "cd #{current_path}; RAILS_ENV=#{rails_env} bundle exec rake paperclip:refresh class=Attachment"
-    end
-  end
-
 end
 
 desc "Create shared folders."
@@ -134,15 +116,3 @@ task :git_tags, :roles => :db do
   run("cd #{release_path} && git push --tags")
 end
 after "deploy:update_code", "git_tags"
-
-namespace :sidekiq do
-  desc "Start sidekiq"
-  task :start, :roles => lambda { fetch(:sidekiq_role) }, :on_no_matching_servers => :continue do
-    rails_env = fetch(:rails_env, "production")
-    # for_each_process do |pid_file, idx|
-    #   run "cd #{current_path} ; nohup #{fetch(:sidekiq_cmd)} -e #{rails_env} -C #{current_path}/config/sidekiq.yml -i #{idx} -P #{pid_file} >> #{current_path}/log/sidekiq.log 2>&1 &", :pty => false
-    # end
-    run "cd #{current_path} ; nohup #{fetch(:sidekiq_cmd)} -e #{rails_env} -C #{current_path}/config/sidekiq.yml -i 0 -P #{current_path}/tmp/pids/sidekiq.pid -c 1 -q default >> #{current_path}/log/sidekiq.default.log 2>&1 &", :pty => false
-    run "cd #{current_path} ; nohup #{fetch(:sidekiq_cmd)} -e #{rails_env} -C #{current_path}/config/sidekiq.yml -i 1 -P #{current_path}/tmp/pids/sidekiq.pid-1 -c 1 -q geolocation >> #{current_path}/log/sidekiq.geolocation.log 2>&1 &", :pty => false
-  end
-end
