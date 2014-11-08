5f81743a1aab3881096b406ef2da52fb43b73a02
Paul Gruson <p.gruson@gmail.com>
2014-11-08 12:31:39 +0100

    push de ssauvegarde

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  before_filter :get_masters
+  before_filter :get_masters, :set_header
 
   def get_masters
     @masters = Theme.masters
+  end
+
+  def set_header
+    @header = "url('/assets/bg_header.jpg')"
   end
 
 end
--- app/controllers/themes_controller.rb
+++ app/controllers/themes_controller.rb
@@ -2,6 +2,7 @@ class ThemesController < ApplicationController
 
   def show
     @theme = Theme.find(params[:id])
+    @header = @theme.color
   end
 
 
